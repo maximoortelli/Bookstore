76675f37b0e81d2ba51b40cd8d957da8badbbed4
maximoortelli <maximoortelli@hotmail.com>
2023-09-28 14:56:10 -0300

    Update linters
    Update linters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
 import PersonIcon from '@mui/icons-material/Person';
-import { BrowserRouter, Routes, Route, Link, } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route, Link,
+} from 'react-router-dom';
 import HomePage from './components/bookPages/HomePage';
 import CategoriesPage from './components/bookPages/CategoriesPage';
 import './styles.css';
@@ -8,9 +9,7 @@ import './styles.css';
 function App() {
   return (
     <BrowserRouter>
-
     <nav className="navbar-container">
-
       <div className='navbar-content'>
          <h1 className='title-nav'>Bookstore CMS</h1>
          <ul>
@@ -25,7 +24,6 @@ function App() {
              <PersonIcon />
          </div>
       </div>
-
     </nav>
 
      <Routes>
